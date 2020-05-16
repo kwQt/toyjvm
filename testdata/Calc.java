339d8f7408fe88fbadda4c17815da1e27e9d1ff9
kwkt <44708388+kwQt@users.noreply.github.com>
2020-05-16 21:44:21 +0900

    Modify java files

---
+++ testdata/Calc.java
@@ -0,0 +1,12 @@
+package testdata;
+
+public class Calc {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
